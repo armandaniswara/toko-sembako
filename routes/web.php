f714ce169129c72688c6cbb3cddb5306767f4d43
“nvafandi” <nurvan.afandi@gmail.com>
2025-05-08 04:35:26 +0700

    modify routes

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/home', function () {
     return view('home');
 });
+
+
+//Route::get('/home', function () {
+//    return view('home');
+//});
 
 
 Route::middleware('auth')->group(function () {
