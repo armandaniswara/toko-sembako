475f490c65ca0427874f659aab7166327eb5987a
Arman Daniswara <danisswara02@gmail.com>
2025-05-10 01:09:09 +0700

    style modified

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -14,7 +14,7 @@
 </head>
 <body>
 <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark-grey " style="height: 10%">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-light text-decoration-none">
+    <a href="/" class="d-flex align-items-center mb-md-0 me-md-auto link-light text-decoration-none">
         <a href="" class="fs-4 ff-popins text-decoration-none text-white fw-bold fst-italic ms-3">Sembako<span class="text-coffee">Plus.</span></a>
     </a>
 </div>
